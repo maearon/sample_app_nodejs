378b682b37cde2493ffa16c9b04a076c14534c0f
sonhle9 <sonhle9@gmail.com>
2022-04-06 14:42:21 +0700

    https://stackoverflow.com/questions/44704968/visual-studio-code-insert-new-line-at-the-end-of-files

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const compression = require('compression');
 const cors = require('cors');
 const passport = require('passport');
 const httpStatus = require('http-status');
+const path = require('path');
 const config = require('./config/config');
 const morgan = require('./config/morgan');
 const { jwtStrategy } = require('./config/passport');
@@ -13,7 +14,6 @@ const { authLimiter } = require('./middlewares/rateLimiter');
 const routes = require('./routes/v1');
 const { errorConverter, errorHandler } = require('./middlewares/error');
 const ApiError = require('./utils/ApiError');
-const path = require('path');
 
 const app = express();
 
