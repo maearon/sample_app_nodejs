86157e8ee0708a22dae3f0ea23a821daa3e03cbe
sonhle9 <sonhle9@gmail.com>
2022-04-13 10:00:05 +0700

    layouts

--- src/controllers/staticPage.controller.js
+++ src/controllers/staticPage.controller.js
@@ -10,6 +10,21 @@ const home = catchAsync(async (req, res) => {
   res.status(httpStatus.OK).render('static_pages/home', locals);
 });
 
+const help = (req, res) => {
+  res.render('static_pages/help');
+};
+
+const about = (req, res) => {
+  res.render('static_pages/about');
+};
+
+const contact = (req, res) => {
+  res.render('static_pages/contact');
+};
+
 module.exports = {
   home,
+  help,
+  about,
+  contact,
 };
--- src/routes/v1/staticPage.route.js
+++ src/routes/v1/staticPage.route.js
@@ -4,5 +4,8 @@ const staticPageController = require('../../controllers/staticPage.controller');
 const router = express.Router();
 
 router.get('/', staticPageController.home);
+router.get('/help', staticPageController.help);
+router.get('/about', staticPageController.about);
+router.get('/contact', staticPageController.contact);
 
 module.exports = router;
