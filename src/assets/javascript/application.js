3c3f3db877b07e0d76c77cb6873bfaef2e7e473b
sonhle9 <sonhle9@gmail.com>
2022-04-13 14:35:49 +0700

    layouts

---
+++ src/assets/javascript/application.js
@@ -0,0 +1,17 @@
+// This file is automatically compiled by Webpack, along with any other files
+// present in this directory. You're encouraged to place your actual application logic in
+// a relevant structure within app/javascript and only use these pack files to reference
+// that code so it'll be compiled.
+
+// eslint-disable-next-line import/no-unresolved
+import Rails from '@rails/ujs';
+// eslint-disable-next-line import/no-unresolved
+import Turbolinks from 'turbolinks';
+// eslint-disable-next-line import/no-unresolved
+import * as ActiveStorage from '@rails/activestorage';
+// eslint-disable-next-line import/no-unresolved
+import 'channels';
+
+Rails.start();
+Turbolinks.start();
+ActiveStorage.start();
