db1cfdaa49a46549ecce426768690396c72bb8ea
sonhle9 <sonhle9@gmail.com>
2022-04-19 16:27:47 +0700

    fix routes

--- src/controllers/session.controller.js
+++ src/controllers/session.controller.js
@@ -1,8 +1,8 @@
 const httpStatus = require('http-status');
 const catchAsync = require('../utils/catchAsync');
 // const { userService } = require('../services');
-const { User } = require('../models');
-const { authService } = require('../services');
+// const { User } = require('../models');
+// const { authService } = require('../services');
 
 const newSession = catchAsync(async (req, res) => {
   const locals = {
@@ -15,27 +15,27 @@ const newSession = catchAsync(async (req, res) => {
 
 const create = catchAsync(async (req, res) => {
   // const user = await userService.createUser(req.body.session);
-  const user = User.findOne(req.body.session.email.toLowerCase);
-  if (user && user.PasswordDigest(...req.body.session.password)) {
-    if (user.isEmailVerified?) {
-      authService.loginUserWithEmailAndPassword(...req.body.session);
-      req.body.session.remember_me == '1' ? remember(user) : forget(user);
-      res.render('users/show', user);
-    } else {
-      message  = "Account not activated. ";
-      message += "Check your email for the activation link.";
-      // flash[:warning] = message;
-      res.render('static_pages/home');
-    }
-  } else {
-    // flash.now[:danger] = 'Invalid email/password combination'
-    res.status(httpStatus.OK).render('sessions/new');
-  }
-  if (await User.isEmailTaken(userBody.email)) {
-    throw new ApiError(httpStatus.BAD_REQUEST, 'Email has already been taken');
-  }
-  return User.create(userBody);
-  res.render('static_pages/home', user);
+  // const user = User.findOne(req.body.session.email.toLowerCase);
+  // if (user && user.PasswordDigest(...req.body.session.password)) {
+  //   if (user.isEmailVerified?) {
+  //     authService.loginUserWithEmailAndPassword(...req.body.session);
+  //     req.body.session.remember_me == '1' ? remember(user) : forget(user);
+  //     res.render('users/show', user);
+  //   } else {
+  //     message  = "Account not activated. ";
+  //     message += "Check your email for the activation link.";
+  //     flash[:warning] = message;
+  //     res.render('static_pages/home');
+  //   }
+  // } else {
+  //   flash.now[:danger] = 'Invalid email/password combination'
+  //   res.status(httpStatus.OK).render('sessions/new');
+  // }
+  // if (await User.isEmailTaken(userBody.email)) {
+  //   throw new ApiError(httpStatus.BAD_REQUEST, 'Email has already been taken');
+  // }
+  // return User.create(userBody);
+  // res.render('static_pages/home', user);
 
 });
 
--- src/routes/v1/user.route.js
+++ src/routes/v1/user.route.js
@@ -2,7 +2,7 @@ const express = require('express');
 const auth = require('../../middlewares/auth');
 const validate = require('../../middlewares/validate');
 const userValidation = require('../../validations/user.validation');
-const userController = require('../../controllers/user.controller');
+const userController = require('../../controllers/api/user.controller');
 
 const router = express.Router();
 
