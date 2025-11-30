3ffafe36423bc0ea033e9f28a90e781a8458f18d
maearon <manhng132@gmail.com>
2025-11-30 09:52:31 +0700

    refactor: update auth controller to include 'me' endpoint and improve module exports

--- src/validations/index.js
+++ src/validations/index.js
@@ -1,2 +1,7 @@
-module.exports.authValidation = require('./auth.validation');
-module.exports.userValidation = require('./user.validation');
+// module.exports.authValidation = require('./auth.validation');
+// module.exports.userValidation = require('./user.validation');
+
+import authValidation from './auth.validation';
+import userValidation from './user.validation';
+
+export { authValidation, userValidation };
